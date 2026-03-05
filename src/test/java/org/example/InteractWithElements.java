f15e8798971190ff3ea64a721bf9d4364c8170ad
MdTajwarAliRumman <tajwaralirumman@gmail.com>
2026-03-05 15:38:20 +0600

    Testing web elements

--- src/test/java/org/example/InteractWithElements.java
+++ src/test/java/org/example/InteractWithElements.java
@@ -10,13 +10,15 @@ public class InteractWithElements extends BrowserSetup {
     public void testElement() throws InterruptedException {
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
-        WebElement radio_btn = driver.findElement(By.xpath("//input[@value='radio1']"));
-        radio_btn.click();
+        WebElement radio_btn1 = driver.findElement(By.xpath("//input[@value='radio1']"));
+        System.out.println("Radio button1 select State After Click: " + radio_btn1.isSelected()); // isSelected is here to determine if an element is selected or not
+        System.out.println("isEnable" + radio_btn1.isEnabled());
+        radio_btn1.click();
 
         WebElement radio_btn2 = driver.findElement(By.xpath("//input[@value='radio2']"));
 //        radio_btn2.click();
-        System.out.println(radio_btn.isSelected());
-        System.out.println(radio_btn2.isSelected());
+        System.out.println("Radio button1 select State After Click: " + radio_btn1.isSelected()); // isSelected is here to determine if an element is selected or not
+        System.out.println("Radio button2 select State Before Click: " + radio_btn2.isSelected());
 
         Thread.sleep(2000);
     }
