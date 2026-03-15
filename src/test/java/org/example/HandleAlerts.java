9d48207d7d139a4f09f84ffb1bd8e64bc339be5e
MdTajwarAliRumman <tajwaralirumman@gmail.com>
2026-03-15 15:33:08 +0600

    Handle Alerts

--- src/test/java/org/example/HandleAlerts.java
+++ src/test/java/org/example/HandleAlerts.java
@@ -1,4 +1,26 @@
 package org.example;
 
-public class HandleAlerts {
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HandleAlerts extends BrowserSetup {
+
+    By jsAlertButon = By.xpath("//button[normalize-space()='Click for JS Alert']");
+    By jsConfirm = By.xpath("//button[normalize-space()='Click for JS Confirm']");
+    By jsPrompt = By.xpath("//button[normalize-space()='Click for JS Prompt']");
+    By result_txt = By.xpath("//p[@id='result']");
+
+    @Test
+    public void testAlerts() {
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        getElement(jsAlertButon).click();
+        Alert alert = driver.switchTo().alert(); //*******this is how we need to switch to the alert prompt
+        System.out.println(alert.getText());
+        alert.accept();
+        Assert.assertEquals(getElement(result_txt).getText(), "You successfully clicked an alert");
+    }
 }
+
+
