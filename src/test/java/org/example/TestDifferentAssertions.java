9d8f51a2ebe621bf96bb6082221cf71e7d44a951
MdTajwarAliRumman <tajwaralirumman@gmail.com>
2026-03-18 11:33:58 +0600

    Testing Different Assertions

--- src/test/java/org/example/TestDifferentAssertions.java
+++ src/test/java/org/example/TestDifferentAssertions.java
@@ -1,4 +1,32 @@
 package org.example;
 
-public class TestDifferentAssertions {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class TestDifferentAssertions extends BrowserSetup {
+    @Test
+    public void testAssertion() {
+        Assert.assertEquals(1, 1);
+        System.out.println("Step -1 ");
+        Assert.assertFalse(true);
+        System.out.println("Step -2");
+        Assert.assertTrue(true);
+        System.out.println("Step -3");
+    }
+
+    @Test
+    public void softTestAssertion() {
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(1, 1);
+        System.out.println("Step -1 ");
+        softAssert.assertFalse(true);
+        System.out.println("Step -2");
+        softAssert.assertTrue(true);
+        System.out.println("Step -3");
+
+        softAssert.assertAll();
+
+    }
+
 }
